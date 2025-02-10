efdece12af7f19e0b7cec053fc40266902151de4
fadhilrachman <muhfadhilrachman@gmail.com>
2025-02-10 11:35:44 +0700

    finishhhhhhhhhhh

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,16 @@
+"use client";
 import { FiArrowUpRight } from "react-icons/fi";
 import SocialLink from "./SocialLink";
 
 const Contact = () => {
+  const handleDownloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/cv/CV.pdf"; // Path ke file di folder public
+    link.download = "CV_Fadhil.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div className="wrapper">
       <div className="max-w-5xl mx-auto flex flex-col gap-4 items-center justify-center">
@@ -11,11 +20,14 @@ const Contact = () => {
           presence, or helping with Your website design and converting
           challenges.
         </p>
-        <a href="mailto:renaldirendi134@gmail.com">
-          <button className="w-52 h-14 bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-950 text-xl font-bold text-white rounded-lg hover:from-indigo-950 hover:to-blue-600 duration-300">
-            Download My CV
-          </button>
-        </a>
+        <button
+          onClick={() => {
+            handleDownloadCV();
+          }}
+          className="w-52 h-14 bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-950 text-xl font-bold text-white rounded-lg hover:from-indigo-950 hover:to-blue-600 duration-300"
+        >
+          Download My CV
+        </button>
       </div>
       <div className="mt-8 flex items-center flex-wrap   gap-8 justify-center">
         <SocialLink title="Github" link="https://github.com/fadhilrachman" />
@@ -32,7 +44,7 @@ const Contact = () => {
           // link="https://www.linkedin.com/in/muhammad-fadhil-rahman-856096247/"
         />
       </div>
-      <div className="flex justify-center items-center space-x-5 mt-4">
+      <div className="flex flex-col md:flex-row justify-center items-center space-x-5 mt-4">
         <p>muhfadhilrachman@gmail.com</p>
         <p>+62881022077883</p>
       </div>
